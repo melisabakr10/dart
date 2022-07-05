4eb4e6ed3451614ea6aae113cfba304b37030019
melisabakr10 <bakirmelisa1010@gmail.com>
2022-07-05 12:58:37 +0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,8 +1,8 @@
 // 192855037 - İlhami Tuğral
 
-String university = 'Uşak Üniversitesi';
+String university = '----GEDİK ÜNİVERSİTESİ';
 
 void main() {
-  print("${university}'ne hoşgeldiniz");
+  print("${university}'ne hoşgeldiniz----");
   print("yeap");
 }
